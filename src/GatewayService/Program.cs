92296693644f8d210cda543843788850b13c7d93
Mohamed A. Laghrour <35415121+cppJoker@users.noreply.github.com>
2023-01-19 13:57:12 -0500

    code cleanup

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,7 +1,4 @@
 using CommonLibrary.AspNetCore.Core;
-using CommonLibrary.Core;
-using CommonLibrary.Logging.Models.Dtos;
-using GatewayService.Logging.Implementations;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +8,6 @@ var logger = new LoggerConfiguration().WriteTo.Console();
 builder.Services.AddCommonLibrary(builder.Configuration, builder.Logging, logger , MyAllowSpecificOrigins);
 builder.Services.AddCommonLibraryLoggingService();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IRepository<LogHandleDto>, LogHandleRepository>();
 var app = builder.Build();
 app.UseCommonLibrary(MyAllowSpecificOrigins);
 if (app.Environment.IsDevelopment())
